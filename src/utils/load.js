709578497f0efcbc41f00a3a9b34fa84a8f328d0
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2025-06-27 15:23:44 +0300

    Update load.js

--- src/utils/load.js
+++ src/utils/load.js
@@ -9,13 +9,13 @@ const log = debug('page-loader')
 
 export default (url, responseType = 'json') => {
   log(`Start loading - ${url}`)
-    const controller = new AbortController()
-    const timerId = setTimeout(()=> controller.abort(), 120000)
+  const controller = new AbortController()
+  const timerId = setTimeout(() => controller.abort(), 120000)
   return axios({
     method: 'get',
     url,
     responseType,
-    signal: controller.signal
+    signal: controller.signal,
   }).then((response) => {
     clearTimeout(timerId)
     log(`Received response from ${response.config.url} with status ${response.status}`)
@@ -23,12 +23,11 @@ export default (url, responseType = 'json') => {
   })
     .catch((e) => {
       clearTimeout(timerId)
-      if(e.name === 'AbortError'){
+      if (e.name === 'AbortError') {
         log('Request timed out after 120s')
-             throw new Error(e.message)
+        throw new Error(e.message)
       }
       log(`Error '${e.message}' when loading ${url}`)
       throw new Error(e.message)
     })
-
 }
